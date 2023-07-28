821c987e1daa151b2c64258a97ec4c146d60811e
irkuhs <ahmadshukri0911@gmail.com>
2023-07-28 10:35:17 +0800

    feat: blog controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,37 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use Str;
 use App\Models\Blog;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $blogs = Blog::all();
+
+        return Inertia::render(
+            'Blogs/Index',
+            [
+                'blogs' => $blogs
+            ]
+        );
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return Inertia::render(
+            'Blogs/Create'
+        );
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+        Blog::create([
+            'title' => $request->title,
+            'slug' => \Str::slug($request->slug),
+            'content' => $request->content
+        ]);
+        sleep(1);
+
+        return redirect()->route('blogs.index')->with('message', 'Blog Created Successfully');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
      */
     public function show(Blog $blog)
     {
@@ -41,25 +74,56 @@ class BlogController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
      */
     public function edit(Blog $blog)
     {
-        //
+        return Inertia::render(
+            'Blogs/Edit',
+            [
+                'blog' => $blog
+            ]
+        );
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Blog $blog)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        $blog->title = $request->title;
+        $blog->slug = Str::slug($request->slug);
+        $blog->content = $request->content;
+        $blog->save();
+        sleep(1);
+
+        return redirect()->route('blogs.index')->with('message', 'Blog Updated Successfully');
+
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Blog $blog)
     {
-        //
+        $blog->delete();
+        sleep(1);
+
+        return redirect()->route('blogs.index')->with('message', 'Blog Delete Successfully');
     }
 }
